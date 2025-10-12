7d0321e681ac2661e4bb7eeaf5782422be94acd8
吴涵华 <w5hh@qq.com>
2025-10-12 22:57:52 +0800

    增加maven的属性配置，便于统一修改配置 修改日志输出的目录，以及增加日志文件滚动策略

--- pos-terminal/src/test/java/com/hclm/terminal/MyTest.java
+++ pos-terminal/src/test/java/com/hclm/terminal/MyTest.java
@@ -8,13 +8,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:mysql://192.168.3.2:3306/pos_db?useSSL=false&serverTimezone=Asia/Shanghai&allowPublicKeyRetrieval=true&useSSL=false",
-        "spring.datasource.username=root",
-        "spring.datasource.password=54hanhua@yw.cn",
-        "spring.flyway.enabled=false",
-        "spring.data.redis.host=192.168.3.2"
-})
+@SpringBootTest
 public class MyTest {
     public static void main(String[] args) {
         Instant instant = Instant.now();
